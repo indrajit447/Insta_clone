e73b36118af3d6ebd50f2b6f5ce70651c04fd4bb
indrajit447 <naskarindrajit557@gmail.com>
2023-03-29 22:36:33 +0530

    First commit
    Signed-off-by: indrajit447 <naskarindrajit557@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './Components/Home';
+import SignUp from './Components/SignUp';
+import SignIn from './Components/SignIn';
+import Profile from './Components/Profile';
 
 function App() {
   return (
+    <BrowserRouter>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/>
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/ SignUp" element={<SignUp/>}/>
+      <Route exact path="/SignIn" element={<SignIn/>}/>
+      <Route exact path="/ Profile" element={<Profile/>}/>
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from "../img/logo.png"
+
+export default function Home() {
+  return (
+    <div> Home</div>
+  )
+}
